4ce7fdaad431f837ecd941a32179e00fc2b5bace
The-cloud-achiever <jastivedasri25@gmail.com>
2025-07-02 17:55:29 +0100

    change

--- Groups_test/get_test_with_sort.py
+++ Groups_test/get_test_with_sort.py
@@ -132,7 +132,7 @@ def generate_html_report(snapshot, output_path):
 
     # Sort group names alphabetically
     changed_sorted = sorted(changed_groups.items())
-    unchanged_sorted = sorted(unchanged_groups.items()
+    unchanged_sorted = sorted(unchanged_groups.items())
     
     def append_group_section(groups):
         for group, data in groups:
@@ -144,7 +144,8 @@ def generate_html_report(snapshot, output_path):
                     html.append(f"<tr><td class='{class_name}'>{change_type.capitalize()}</td><td class='{class_name}'>{member}</td></tr>")
             html.append("</table><br>")
 
-    )
+    
+
     # Display groups with changes first
     html.append("<h1>Groups With Changes</h1>")
     if changed_sorted:
